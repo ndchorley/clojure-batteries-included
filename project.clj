21867f4160e62e6aaccf5f20881dafdc7bf7b14b
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2020-03-17 16:32:32 +0000

    Add Langohr for RabbitMQ

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject batteries-included "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [cheshire "5.10.0"]
-                 [clj-http "3.10.0"]]
+                 [clj-http "3.10.0"]
+                 [com.novemberain/langohr "5.1.0"]]
   :plugins [[cider/cider-nrepl "0.24.0"]])
