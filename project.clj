737bf21e4831d1d98596c0994298899238e7f72d
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2020-04-26 17:36:56 +0100

    Add core.async

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [cheshire "5.10.0"]
                  [clj-http "3.10.0"]
-                 [com.novemberain/langohr "5.1.0"]]
+                 [com.novemberain/langohr "5.1.0"]
+                 [org.clojure/core.async "1.1.587"]]
   :plugins [[cider/cider-nrepl "0.24.0"]])
