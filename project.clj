e00e3375d8de2786c8a47134bbec966ae02e4ff2
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2020-08-01 17:57:23 +0100

    Upgrade clj-http

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject batteries-included "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [cheshire "5.10.0"]
-                 [clj-http "3.10.0"]
+                 [clj-http "3.10.1"]
                  [com.novemberain/langohr "5.1.0"]
                  [org.clojure/core.async "1.1.587"]]
   :plugins [[cider/cider-nrepl "0.24.0"]])
