2925fd6589db0481bcf8d609a6a1e8d453ee2a55
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2022-11-27 17:44:37 +0000

    Upgrade Clojure to 1.11.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject batteries-included "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [cheshire "5.10.0"]
                  [clj-http "3.12.1"]
                  [com.novemberain/langohr "5.1.0"]
