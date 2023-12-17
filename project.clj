c0578b791195a4b4b9d2a65461dd4e8b836ab89f
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-12-17 07:30:56 +0000

    Upgrade cider-nrepl to 0.44.0

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
                  [clj-http "3.12.1"]
                  [com.novemberain/langohr "5.1.0"]
                  [org.clojure/core.async "1.1.587"]]
-  :plugins [[cider/cider-nrepl "0.25.5"]])
+  :plugins [[cider/cider-nrepl "0.44.0"]])
